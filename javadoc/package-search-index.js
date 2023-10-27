ef91391570dc66b88f7caa53828692c3dd640b71
jbekhet <94532219+jbekhet@users.noreply.github.com>
2023-10-27 19:28:51 +0000

    Deploying to javadoc from @ TGM-HIT/sew9-2324-worttrainer-jbekhet@28ec3017f1a86e70c5b78e771bc41d41fe0eedee 🚀

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jbekhet.Controller"},{"l":"jbekhet.GrafischeOberflaeche"},{"l":"jbekhet.Model"},{"l":"jbekhet.Persistenz"}];updateSearchResults();
